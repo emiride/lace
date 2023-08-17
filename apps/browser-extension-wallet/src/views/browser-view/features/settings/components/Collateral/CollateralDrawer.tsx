41271121e8fff2d4def14e1f2060365486049f6f
John Oshalusi <oshalusijohn@gmail.com>
2023-08-17 13:40:18 +0100

    fix: replace `visible` prop with `open` on modals, tooltips and drawers (#385)

--- apps/browser-extension-wallet/src/views/browser-view/features/settings/components/Collateral/CollateralDrawer.tsx
+++ apps/browser-extension-wallet/src/views/browser-view/features/settings/components/Collateral/CollateralDrawer.tsx
@@ -146,7 +146,7 @@ export const CollateralDrawer = ({
 
   return (
     <Drawer
-      visible={visible}
+      open={visible}
       onClose={handleClose}
       title={<DrawerHeader popupView={popupView} title={t('browserView.settings.wallet.collateral.title')} />}
       navigation={
