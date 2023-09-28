b6893e827cfe35e2a247d69675878887bdc8f930
przemyslaw.wlodek <przem.wlodek.github@gmail.com>
2023-09-28 17:28:12 +0200

    feat(staking): add PoolDetailsCard style improvements

--- packages/staking/src/features/drawer/preferences/PoolDetailsCard/PoolDetailsCard.tsx
+++ packages/staking/src/features/drawer/preferences/PoolDetailsCard/PoolDetailsCard.tsx
@@ -51,7 +51,7 @@ export const PoolDetailsCard = ({
   }, [onSliderChange, localValue]);
 
   return (
-    <Card.Outlined>
+    <Card.Outlined className={styles.root}>
       <Flex justifyContent="space-between" alignItems="center" my="$24" mx="$32">
         <Flex alignItems="center" gap="$24">
           <Box className={styles.poolIndicator} style={{ backgroundColor: color }} />
@@ -62,33 +62,36 @@ export const PoolDetailsCard = ({
       {expanded && (
         <>
           <Flex className={styles.valuesRow}>
-            <Flex pl="$32" pr="$32" flexDirection="column">
+            <Flex pl="$32" pr="$32" flexDirection="column" className={styles.valueBox}>
               <Box>
                 <Text.Body.Large weight="$medium" className={styles.valueLabel}>
                   {t('drawer.preferences.poolDetails.savedRatio')}
                 </Text.Body.Large>
+                {/* TODO tooltips & styles */}
                 <InfoIcon className={styles.valueInfoIcon} />
               </Box>
               <Text.Body.Large weight="$semibold">
                 {savedRatio || '-'} {savedRatio && <Text.Body.Small weight="$medium">%</Text.Body.Small>}
               </Text.Body.Large>
             </Flex>
-            <Flex pl="$32" pr="$32" flexDirection="column">
+            <Flex pl="$32" pr="$32" flexDirection="column" className={styles.valueBox}>
               <Box>
                 <Text.Body.Large weight="$medium" className={styles.valueLabel}>
                   {t('drawer.preferences.poolDetails.actualRatio')}
                 </Text.Body.Large>
+                {/* TODO tooltips & styles */}
                 <InfoIcon className={styles.valueInfoIcon} />
               </Box>
               <Text.Body.Large weight="$semibold">
                 {actualRatio || '-'} {actualRatio && <Text.Body.Small weight="$medium">%</Text.Body.Small>}
               </Text.Body.Large>
             </Flex>
-            <Flex pl="$32" pr="$32" flexDirection="column">
+            <Flex pl="$32" pr="$32" flexDirection="column" className={styles.valueBox}>
               <Box>
                 <Text.Body.Large weight="$medium" className={styles.valueLabel}>
                   {t('drawer.preferences.poolDetails.actualStake')}
                 </Text.Body.Large>
+                {/* TODO tooltips & styles */}
                 <InfoIcon className={styles.valueInfoIcon} />
               </Box>
               <Text.Body.Large weight="$semibold">
